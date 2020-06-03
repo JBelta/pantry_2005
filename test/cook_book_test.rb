b8d6771ac02b1dc6751f4abc23ccaead04635d38
James Belta <james.belta@gmail.com>
2020-06-03 11:23:50 -0600

    Edit setup for iter 3

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,6 +7,27 @@ require 'minitest/pride'
 
 class CoookBookTest < Minitest::Test
   def setup
+    @ingredient1 = Ingredient.new({
+                                  name: "Cheese",
+                                  unit: "C",
+                                  calories: 50
+                                  })
+    @ingredient2 = Ingredient.new({
+                                  name: "Macaroni",
+                                  unit: "oz",
+                                  calories: 200
+                                  })
+    @ingredient3 = Ingredient.new({
+                                  name: "Ground Beef",
+                                  unit: "oz",
+                                  calories: 100
+                                  })
+    @ingredient4 = Ingredient.new({
+                                  name: "Bun",
+                                  unit: "g",
+                                  calories: 75
+                                  })
+
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
