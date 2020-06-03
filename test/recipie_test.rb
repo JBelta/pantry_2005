f065e695a4bcc5f11cbeb9da3d807feb016fcae9
James Belta <james.belta@gmail.com>
2020-06-03 10:57:43 -0600

    Build Recipe test with setup and instance assertion, build recipe class

--- test/recipie_test.rb
+++ test/recipie_test.rb
@@ -1,9 +1,9 @@
-require './lib/pantry'
 require './lib/ingredient'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class PantryTest < Minitest::Test
+class RecipeTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({
                                   name: "Cheese",
@@ -15,26 +15,10 @@ class PantryTest < Minitest::Test
                                   unit: "oz",
                                   calories: 200
                                   })
-    @pantry =  Pantry.new
+    @recipe1 = Recipe.new("Mac and Cheese")
   end
 
   def test_does_it_exist
-    assert_instance_of Pantry, @pantry
-  end
-
-  def test_pantry_has_attributes
-    assert_equal ({}), @pantry.stock
-  end
-
-  def test_stock_check
-    assert_equal 0, @pantry.stock_check(@ingredient1)
-  end
-
-  def test_restock
-    @pantry.restock(@ingredient1, 5)
-    @pantry.restock(@ingredient1, 10)
-    @pantry.restock(@ingredient2, 7)
-    assert_equal 15, @pantry.stock_check(@ingredient1)
-    assert_equal 7, @pantry.stock_check(@ingredient2)
+    assert_instance_of Recipe, @recipe1
   end
 end
